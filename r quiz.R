65ca29ab9a0b324ed2056184359589b07c1bfa8d
XichavoAndronica <4114603@myuwc.ac.za>
2021-04-20 13:20:41 -0700

    first commit

---
+++ r quiz.R
@@ -0,0 +1,32 @@
+#Day_2_R_QUIZ
+
+QUESTION 1
+
+numerical data-data that can be objectively counted, measured or calculated. eg discrete data
+Continuous data-data the represent measured things
+qualitative data-well defined categories, including descriptive words. eg categorical data
+Ordinal data
+binary data-data in which the observations can take either one of two outcomes
+missing values
+character values-data presented not in numbers but human words
+
+library(tidyverse)
+Head()
+tail()
+colnames()
+summary()
+
+Skewness is a measure of the symmetry of the distribution of the data. a negative skewness shows that the mean of the data is less than the median and a positive skewness shows that the mean is greater than the median
+Kurtosis describes the tail shape of the data distribution. zero kurtosis means normal data. negative kurtosis form a thin tail, positive kurtosis result in fat tail
+
+question 2
+
+library(tidyverse)
+Orange 
+
+question 3
+
+Mutate() means you are creating new columns on the dataset
+select()
+group_by() means you are comparing two datasets of the same kind
+filter() means
