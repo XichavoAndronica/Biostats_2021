515843e61396fde23df5ee914cce3348a2c639ac
XichavoAndronica <4114603@myuwc.ac.za>
2021-04-21 11:05:20 -0700

    commit

---
+++ Day_3.R
@@ -0,0 +1,28 @@
+#Day_3
+#AJ Smit
+#21 April 2021
+
+
+library(tidyverse)
+head(faithful)
+faithful <- datasets::faithful
+
+eruption.lm <- lm(eruptions ~ waiting, data = faithful)
+summary(eruption.lm)
+
+slope <- round(eruption.lm$coef[2], 3)
+
+slope <- round(eruption.lm$coef[2], 3)
+# p.val <- round(coefficients(summary(eruption.lm))[2, 4], 3) # it approx. 0, so...
+p.val = 0.001
+r2 <- round(summary(eruption.lm)$r.squared, 3)
+ggplot(data = faithful, aes(x = waiting, y = eruptions)) +
+  geom_point() +
+  annotate("text", x = 45, y = 5, label = paste0("slope == ", slope, "~(min/min)"), parse = TRUE, hjust = 0) +
+  annotate("text", x = 45, y = 4.75, label = paste0("italic(p) < ", p.val), parse = TRUE, hjust = 0) +
+  annotate("text", x = 45, y = 4.5, label = paste0("italic(r)^2 == ", r2), parse = TRUE, hjust = 0) +
+  stat_smooth(method = "lm", colour = "salmon") +
+  labs(title = "Old Faithful eruption data",
+       subtitle = "Linear regression",
+       x = "Waiting time (minutes)",
+       y = "Eruption duration (minutes)")
